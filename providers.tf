c00553ae102e1c00430a38e2fb817f1fd14e5c29
Luan Vieira <luandasilvavieira@hotmail.com>
2025-09-27 14:56:13 -0300

    fix

--- providers.tf
+++ providers.tf
@@ -9,9 +9,9 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name   = var.resource_group_name
-    storage_account_name  = var.storage_account_name
-    container_name        = var.container_name
+    resource_group_name   = ""
+    storage_account_name  = ""
+    container_name        = ""
     key                   = "key=cluster-aks.tfstate"
   }
 }
