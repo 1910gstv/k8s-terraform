c700fd21327e44bdaac487f2812a614fba6ff12c
1910gstv <gustavo.hpaula@outlook.com>
2025-09-27 14:35:37 -0300

    variables

--- providers.tf
+++ providers.tf
@@ -8,12 +8,7 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.storage_container_name
-    key                  = "cluster-aks.tfstate"
-  }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
